33535fbf3b562a2e2667560e7ec18f4655e20516
1Yamileth <123503633+1Yamileth@users.noreply.github.com>
2023-10-02 20:40:23 -0600

    Clase 7
    Vimos conceptos de POO: Abstracción, polimorfismo y demás definiciones.
    Vimos los métodos asíncronos y porque son buenos.

---
+++ Modulo1/Vista/Form2.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Vista
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SincronoButton = new Button();
+            AsincronoButton = new Button();
+            SumanButton = new Button();
+            SuspendLayout();
+            // 
+            // SincronoButton
+            // 
+            SincronoButton.Location = new Point(138, 43);
+            SincronoButton.Name = "SincronoButton";
+            SincronoButton.Size = new Size(150, 104);
+            SincronoButton.TabIndex = 0;
+            SincronoButton.Text = "Hornear pollo";
+            SincronoButton.UseVisualStyleBackColor = true;
+            SincronoButton.Click += SincronoButton_Click;
+            // 
+            // AsincronoButton
+            // 
+            AsincronoButton.Location = new Point(410, 43);
+            AsincronoButton.Name = "AsincronoButton";
+            AsincronoButton.Size = new Size(150, 104);
+            AsincronoButton.TabIndex = 1;
+            AsincronoButton.Text = "Hornear Pollo";
+            AsincronoButton.UseVisualStyleBackColor = true;
+            AsincronoButton.Click += AsincronoButton_Click;
+            // 
+            // SumanButton
+            // 
+            SumanButton.Location = new Point(234, 246);
+            SumanButton.Name = "SumanButton";
+            SumanButton.Size = new Size(150, 104);
+            SumanButton.TabIndex = 2;
+            SumanButton.Text = "Sumar";
+            SumanButton.UseVisualStyleBackColor = true;
+            SumanButton.Click += SumanButton_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(969, 526);
+            Controls.Add(SumanButton);
+            Controls.Add(AsincronoButton);
+            Controls.Add(SincronoButton);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button SincronoButton;
+        private Button AsincronoButton;
+        private Button SumanButton;
+    }
+}
---
+++ Modulo1/Vista/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void SincronoButton_Click(object sender, EventArgs e)
+        {
+            HornearPollo();
+            MessageBox.Show("Listo, Esta El Pollo Hecho");
+        }
+
+        private void AsincronoButton_Click(object sender, EventArgs e)
+        {
+            HornearPolloAsync();
+            MessageBox.Show("Listo, Esta El Pollo Hecho");
+        }
+
+        private void HornearPollo()
+        {
+            Thread.Sleep(8000);
+        }
+
+        private Task HornearPolloAsync()
+        {
+            return Task.Delay(8000);
+        }
+
+
+        private async Task<decimal> SumarAsync(decimal num1, decimal num2)
+        {
+            decimal suma = await Task.Run(() => /*El "await" lo que hace es esperar a que la tarea finalice, una vez que termine se amnda a travez de la variable*/
+            {
+                return num1 + num2;
+            });
+            return suma;
+        }
+
+        private async void SumanButton_Click(object sender, EventArgs e)
+        {
+            decimal resultado = await  SumarAsync(5, 20); //el llamado de metodos asincronos se hace con el "await" siempre
+
+            MessageBox.Show($"La suma es: {resultado} ");
+        }
+    }
+}
